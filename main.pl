233db09641b3906fdcfa670d97b05598e4788b67
Satyam-53 <66866618+Satyam-53@users.noreply.github.com>
2021-10-02 22:44:28 +0530

    Create main.pl

---
+++ main.pl
@@ -0,0 +1,43 @@
+/*
+NAME: 
+ID: 
+*/
+
+age(rohan, 23).
+citizen(rohan, 23).
+age(meera, 30).
+citizen(meera, 8).
+age(david, 35).
+citizen(david, 35).
+age(leonard, 40).
+citizen(leonard, 40).
+age(amy, 38).
+citizen(amy, 5).
+
+stateOfUS(newHampshire).
+stateOfUS(massachusetts).
+stateOfUS(connecticut).
+stateOfUS(newYork).
+stateOfUS(newJersey).
+stateOfUS(pennsylvania).
+stateOfUS(delaware).
+stateOfUS(maryland).
+stateOfUS(virginia).
+stateOfUS(northCarolina).
+stateOfUS(southCarolina).
+stateOfUS(georgia).
+
+monday(7, 1, 2019).
+monday(2, 12, 2019).
+
+% ARTICLE 1
+% Section 1
+
+
+% Section 2
+----------- Statements for Section 2------------------------------------------
+…
+…
+...
+----------- Statements for Amendment 27----------------------------------
+% END
