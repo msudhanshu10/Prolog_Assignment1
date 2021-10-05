e6e6b7e686ed85c022e962254227c3516b72031d
Satyam-53 <66866618+Satyam-53@users.noreply.github.com>
2021-10-05 21:37:49 +0530

    Update main.pl

--- main.pl
+++ main.pl
@@ -1,4 +1,6 @@
 /*
+NAME: Satyam Srivastava
+ID: 2019B1A70188P
 NAME: Sudhanshu Mishra
 ID: 2019B1A70750P
 */
@@ -33,6 +35,30 @@ monday(2, 12, 2019).
 % ARTICLE 1
 % Section 1
 
+%Section 2
+houseOfRepresentatives(X) :- age(X,Y), Y>=20,
+				citizen(X,Z), Z>=7.
+qualified(X, houseOfRepresentatives) :- houseOfRepresentatives(X).
+
+%Section 8
+power(congress, "To borrow Money on the credit of the United States").
+power(congress, "To regulate Commerce with foreign Nations, and among 
+the several States, and with the Indian Tribes").
+power(congress, "To establish an uniform Rule of Naturalization, and uniform Laws on the subject of Bankruptcies throughout the United States").
+power(congress, "To coin Money, regulate the Value thereof, and of foreign Coin, and fix the Standard of Weights and Measures").
+power(congress, "To provide for the Punishment of counterfeiting the Securities and current Coin of the United States").
+power(congress, "To establish Post Offi ces and post Roads").
+power(congress, "To promote the Progress of Science and useful Arts, by securing for limited Times to Authors and Inventors the exclusive Right to their respective Writings and Discoveries").
+power(congress, "To constitute Tribunals inferior to the supreme Court").
+power(congress, "To define and punish Piracies and Felonies committed on the high Seas, and Offenses against the Law of Nations").
+power(congress, "To declare War, grant Letters of Marque and Reprisal, and make Rules concerning Captures on Land and Water").
+power(congress, "To raise and support Armies, but no Appropriation of Money to that Use shall be for a longer Term than two Years").
+power(congress, "To provide and maintain a Navy").
+power(congress, "To make Rules for the Government and Regulation of the land and naval Forces").
+power(congress, "To provide for calling forth the Militia to execute the Laws of the Union, suppress Insurrections and repel Invasions").
+power(congress, "To provide for organizing, arming, and disciplining, the Militia, and for governing such Part of them as may be employed in the Service of the United States, reserving to the States respectively, the Appointment of the Offi cers, and the Authority of training the Militia according to the discipline prescribed by Congress").
+power(congress, "To exercise exclusive Legislation in all Cases whatsoever, over such District (not exceeding ten Miles square) as may, by Cession of particular States, and the Acceptance of Congress, become the Seat of the Government of the United States, and to exercise like Authority over all Places purchased by the Consent of the Legislature of the State in which the Same shall be, for the Erection of Forts, Magazines, Arsenals, dock-Yards and other needful Building").
+power(congress, "To make all Laws which shall be necessary and proper for carrying into Execution the foregoing Powers, and all other Powers vested by this Constitution in the Government of the United States, or in any Department or Officer thereof.").	
 
 % Section 2
 ----------- Statements for Section 2------------------------------------------
